8762ebaaebb9adbfb4094e965f8fd2261f3c65c1
Yetunde Mogaji <ytee.dev@gmailcom>
2026-01-01 20:45:13 +0100

    Initial Commit

---
+++ vuejs/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navigation from './pages/Navigation.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#f8fafd] px-2">
+    <Navigation />
+    <div>
+      <RouterView />
+    </div>
+  </div>
+</template>
---
+++ vuejs/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import EmailList from './EmailList.vue'
+import MainMenu from './MainMenu.vue'
+</script>
+
+<template>
+  <div class="flex h-screen w-full">
+    <div class="w-60 shrink-0">
+      <MainMenu />
+    </div>
+
+    <div class="flex-1 rounded-3xl bg-white">
+      <EmailList />
+    </div>
+  </div>
+</template>
